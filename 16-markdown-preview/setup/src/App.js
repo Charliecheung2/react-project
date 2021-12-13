3199b5ad4b7582791c0590c1346b0a78ec41be8e
gaojian <jiangao@tuputech.com>
2021-12-14 00:15:50 +0800

    md-finished

--- 16-markdown-preview/setup/src/App.js
+++ 16-markdown-preview/setup/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [text, setText] = useState("## markdown preview");
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
