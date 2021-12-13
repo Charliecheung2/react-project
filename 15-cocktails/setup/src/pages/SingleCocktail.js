a57cb8c962b479fc8fa1eecd246d5f4fa8ed47bc
Iris-kbt <93909447+Iris-kbt@users.noreply.github.com>
2021-12-13 18:13:08 +0800

    initial commit

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -4,10 +4,105 @@ import { useParams, Link } from "react-router-dom";
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    const fetchData = async function () {
+      const response = await fetch(`${url}${id}`);
+      const data = await response.json();
+      if (data.drinks) {
+        const {
+          strDrink: name,
+          strDrinkThumb: image,
+          strAlcoholic: info,
+          strCategory: category,
+          strGlass: glass,
+          strInstructions: instruction,
+          strIngredient1,
+          strIngredient2,
+          strIngredient3,
+          strIngredient4,
+          strIngredient5
+        } = data.drinks[0];
+        setCocktail({
+          name,
+          image,
+          info,
+          category,
+          glass,
+          instruction,
+          ingredient: [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5
+          ]
+        });
+      } else {
+        setCocktail(null);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [id]);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (!cocktail) {
+    return <h2 className="section-title">no cocktail to display</h2>;
+  }
+  const {
+    name,
+    image,
+    info,
+    category,
+    glass,
+    instruction,
+    ingredient
+  } = cocktail;
   return (
-    <div>
-      <h2>single cocktailssss </h2>
-    </div>
+    <section className="section cocktail-section">
+      <Link to="/" className="btn btn-primary">
+        back to list
+      </Link>
+      <h2 className="section-title">{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name :</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category :</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info :</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass :</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instruction :</span>
+            {instruction}
+          </p>
+          <p>
+            <span className="drink-data">ingredient :</span>
+            {ingredient.map((item, index) => {
+              return item ? <span key={index}> {item}</span> : null;
+            })}
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
