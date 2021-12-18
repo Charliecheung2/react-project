6fbf14ad00befd2b511efa90fe70b078322307bd
gaojian <jiangao@tuputech.com>
2021-12-18 21:06:04 +0800

    pagination finished

--- 18-pagination/setup/src/App.js
+++ 18-pagination/setup/src/App.js
@@ -1,8 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+  console.log(data);
+
+  const [page, setPage] = useState(1);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setPeople(data[page - 1]);
+  }, [loading, page]);
+
+  const handlePrev = () => {
+    setPage((old) => {
+      if (old === 1) {
+        return data.length;
+      }
+      return old - 1;
+    });
+  };
+
+  const handleNext = () => {
+    setPage((old) => {
+      if (old === data.length) {
+        return 1;
+      }
+      return old + 1;
+    });
+  };
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "Pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {people.map((person) => {
+            return <Follower key={person.id} {...person} />;
+          })}
+        </div>
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={handlePrev}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  className={`page-btn ${index + 1 === page && "active-btn"}`}
+                  key={index}
+                  onClick={() => setPage(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={handleNext}>
+              next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 18-pagination/setup/src/useFetch.js
+++ 18-pagination/setup/src/useFetch.js
@@ -1,20 +1,34 @@
-import { useState, useEffect } from 'react'
-import paginate from './utils'
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+import { useState, useEffect } from "react";
+import paginate from "./utils";
+const url = "https://api.github.com/users/aristocratos/followers?per_page=100";
 
 export const useFetch = () => {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const separateData = (data) => {
+    const peoplePerPage = 12;
+    let pages = Math.ceil(data.length / peoplePerPage);
+    const newData = Array.from({ length: pages }, (_, i) => {
+      let start = peoplePerPage * i;
+      return data.slice(start, start + peoplePerPage);
+    });
+    return newData;
+  };
 
   const getProducts = async () => {
-    const response = await fetch(url)
-    const data = await response.json()
-    setData(data)
-    setLoading(false)
-  }
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(separateData(data));
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    getProducts()
-  }, [])
-  return { loading, data }
-}
+    getProducts();
+  }, []);
+  return { loading, data };
+};
